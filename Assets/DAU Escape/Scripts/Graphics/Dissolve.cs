e0cc0b5b98034b7dd6aa2455c56e0198c22643a2
angela-gervino <angervino@gmail.com>
2026-02-22 10:21:28 -0600

    Updated Monkey so that it slowly becomes invisible after being defeated before the game object is deleted

--- Assets/DAU Escape/Scripts/Graphics/Dissolve.cs
+++ Assets/DAU Escape/Scripts/Graphics/Dissolve.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,40 @@ namespace DAUEscape
 {
     public class Dissolve : MonoBehaviour
     {
-        public float dissolveTime = 3.0f; // seconds
+        public float dissolveTime = 2.0f; // seconds
+
+        private Renderer[] renderers;
 
         public void Awake() // called as soon as ragdoll is instantiated
         {
             // Time.time = the time at the beginning of the current frame in seconds since the application (game) started
             dissolveTime += Time.time;
-        }
+
+            renderers = GetComponentsInChildren<Renderer>(true);
+        }// Awake
+
 
         public void Update()
         {
-            if (Time.time >= dissolveTime)
+            if (Time.time < dissolveTime)
+            {
+                double alpha = 1 - Math.Pow(Time.time / dissolveTime, 2); // want alpha to go to 0 as the time gets closer to dissolveTime
+
+                foreach (Renderer rend in renderers)
+                {
+                    foreach (Material mat in rend.materials)
+                    {
+                        Color original = mat.color;
+                        mat.color = new Color(original[0], original[1], original[2], (float)alpha);
+                    }
+                }
+            }
+            else
             {
                 Destroy(gameObject);
             }
-        }
+        }// Update
+
     }
 }
 
